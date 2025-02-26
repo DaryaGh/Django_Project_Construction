4ba78ac3b05c451f7286ec11748c91b765a5e2ef
Darya Ghaffary <D.ghaffary@hotmail.com>
2025-02-26 13:49:29 +0330

    Feat : make first Home model in url

--- Construction/urls.py
+++ Construction/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myConstruction.views import Home
 
 urlpatterns = [
+    path('', Home , name='home'),
+
     path('admin/', admin.site.urls),
+
     path('myConstruction/', include('myConstruction.urls')),
 ]
