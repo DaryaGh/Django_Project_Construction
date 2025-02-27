367fc640163126336cd679292c5f47054aff307b
Darya Ghaffary <D.ghaffary@hotmail.com>
2025-02-27 15:00:04 +0330

    Feat :(refactoring) update basic url

--- myConstruction/urls.py
+++ myConstruction/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from myConstruction import views
+# from myConstruction import views
+from .views import *
+
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('About-us', about_us, name='about_us'),
+    path('Contact-us', contact_us, name='contact_us'),
+    path('Projects', projects, name='projects'),
+    path('Projects/<int:id>',project_details, name='project-details'),
+    path('News', news, name='news'),
+    path('News/<int:id>',news_details, name='news-details'),
+    path('Services', services, name='services'),
+    path('Blog', blog, name='blog'),
+
 ]
