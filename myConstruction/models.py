0e9087a1ef5aad0ae2ec159b0dcb981b49cbacab
Darya Ghaffary <D.ghaffary@hotmail.com>
2025-02-27 14:48:31 +0330

    Feat : update basic models

--- myConstruction/models.py
+++ myConstruction/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    pass
+
+
+class ProjectType(models.Model):
+    pass
+
+
+class Project(models.Model):
+    pass
+
+
+class News(models.Model):
+    pass
+
+
+class Service(models.Model):
+    pass
+
+
+class ServiceDetail(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Team(models.Model):
+    pass
+
+
+class Statistic(models.Model):
+    pass
+
+
+class ContactUs(models.Model):
+    pass
+
+
+class Blog(models.Model):
+    pass
+
+
+class BlogDetail(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class setting(models.Model): # jadval tak record(title , link safahat ejtemi, tarikhche ,....)
+    pass
