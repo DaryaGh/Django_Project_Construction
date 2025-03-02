0020b022b845079b64f7d199310d786961da6d1c
Darya Ghaffary <D.ghaffary@hotmail.com>
2025-03-02 14:44:13 +0330

    Feat : write def models in views

--- myConstruction/views.py
+++ myConstruction/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from myConstruction.models import *
 
-# Create your views here.
+
+def Home(request):
+    return HttpResponse("Hello My Second WebSite")
+
+def services(request):
+    return HttpResponse(" ")
+
+def projects(request):
+    return HttpResponse(" ")
+
+def project_details(request, id):
+    return HttpResponse(" ")
+
+def news(request):
+    return HttpResponse(" ")
+
+def news_details(request, id):
+    return HttpResponse(" ")
+
+def contact_us(request):
+    return HttpResponse(" ")
+
+def about_us(request):
+    return HttpResponse(" ")
