0cb61f679b34cd341a8c35ade66f25f0c5559bf7
Darya Ghaffary <D.ghaffary@hotmail.com>
2025-03-02 14:33:47 +0330

    Feat : register some models

--- myConstruction/admin.py
+++ myConstruction/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+# from .models import User, Category, News, Comment, Tag, Image,Customer,Project,Service,Review ,Team ,Statistic ,ContactUs, Setting ,Movie,ProjectType
+from myConstruction.models import *
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(News)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Customer)
+admin.site.register(Project)
+admin.site.register(ProjectType)
+admin.site.register(Image)
+admin.site.register(Service)
+# admin.site.register(Review)
+admin.site.register(ContactUs)
+# admin.site.register(Team)
+# admin.site.register(Statistic)
+admin.site.register(Setting)
+admin.site.register(Movie)
