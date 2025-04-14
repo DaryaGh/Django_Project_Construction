f696f8126667521c8a38c1365f7156304dbb5121
Darya Ghaffary <D.ghaffary@hotmail.com>
2025-04-14 17:07:08 +0430

    ProjectType

--- myConstruction/models.py
+++ myConstruction/models.py
@@ -68,7 +68,12 @@ class Customer(models.Model):
 
 
 class ProjectType(models.Model):
-    pass
+    name = models.CharField(max_length=100, unique=True, blank=True, null=True)
+    sort = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Project(models.Model):
